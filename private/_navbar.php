4b712005173f0f41388577d1d3e4fcd45b547868
ayush <lineage.ayush@outlook.com>
2022-04-20 09:59:26 +0530

    Logo created and navbar design updated

--- private/_navbar.php
+++ private/_navbar.php
@@ -1,21 +1,21 @@
 <?php
-echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+echo '<nav class="navbar navbar-expand-lg">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">CodeX</a>
+  <a class="navbar-brand" href="#">
+  <img src="img/CodeX_logo1.png" height="50" width="60">
+  </a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link active" aria-current="page" href="#">
+      Home</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
+          Categories
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <li><a class="dropdown-item" href="#">Action</a></li>
@@ -25,12 +25,13 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+        <a class="nav-link">Contact Us</a>
       </li>
     </ul>
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <button class="btn btn-secondary" type="submit">Search</button>
+      <button class="btn btn-outline-light mx-3">Login</button>
     </form>
   </div>
 </div>
