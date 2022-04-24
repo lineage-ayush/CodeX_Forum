3b023390f928d056f778b4b00025b7049574b850
ayush <lineage.ayush@outlook.com>
2022-04-24 20:29:39 +0530

    Mde connection with server

--- signup.php
+++ signup.php
@@ -1,3 +1,14 @@
+<? php
+if($_SERVER == ["REQUEST_METHOD"] == "POST"){
+    include'private/_dbconnect.php';
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $cpassword = $_POST["cpassword"];
+    $sql = "INSERT INTO `users` (`email`, `password`) VALUES ('$email', '$password')";
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,21 +19,27 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="codex.css" />
-    <title>CodeX Forum</title>
+    <title>Signup - CodeX Forum</title>
     <style>
         .login {
-            margin-top: 9em;
+            margin-top: 8em;
         }
     </style>
 </head>
 
 <body>
     <?php include'private/_navbar.php'?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success</strong> Your account has been created.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
     <div class="container login col-md-4">
+        <h3>Signup to CodeX Forum</h3>
+
         <form action="signup.php" method ="post">
             <div class="mt-3 mb-2">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter your Email address" />
+                <input type="email" class="form-control" name="email" id="email" placeholder="Enter your Email address" />
             </div>
             <div class="mb-1">
                 <label for="password" class="form-label">Password</label>
@@ -33,7 +50,8 @@
                 <input type="password" class="form-control" name="cpassword" placeholder="*******" id="cpassword" />
             </div>
 
-            <a href="index.php" button type="submit" class="btn btn-primary mt-3">Signup</buuton> </a>
+            <button type="submit" class="btn btn-primary mt-3">Signup</button>
+            <p>Already have an account? <a href="login.php">Login here</p></a>
         </form>
     </div>
 
